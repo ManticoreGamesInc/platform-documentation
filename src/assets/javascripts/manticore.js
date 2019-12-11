cdb082c268451984e611f59af05e312257ba4e8d
Benjamin Staneck <staneck@gmail.com>
2019-12-11 05:29:44 +0100

    Add a new feature that changes the browser tab title based on scroll position

--- src/assets/javascripts/manticore.js
+++ src/assets/javascripts/manticore.js
@@ -1,7 +1,9 @@
-// Load Videos in the Docs
+// Manticore JavaScript Helpers
 
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Load Videos in the Docs
     var elements = document.querySelectorAll('img[alt="YOUTUBE"]');
+    var elementsLive = document.querySelectorAll('img[alt="YOUTUBELIVE"]');
 
     Array.prototype.forEach.call(elements, function(el, i){
         var id = el.getAttribute('src').split('/')[el.getAttribute('src').split('/').length - 1];
@@ -19,8 +21,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         el.replaceWith(iframe);
     });
 
-    var elementsLive = document.querySelectorAll('img[alt="YOUTUBELIVE"]');
-
     Array.prototype.forEach.call(elementsLive, function(el, i){
         var id = el.getAttribute('src').split('/')[el.getAttribute('src').split('/').length - 1];
         var old_class = el.getAttribute('class');
@@ -36,8 +36,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         el.replaceWith(iframe);
     });
+
+    // Change browser tab title based on header title near scroll position
+
+    window.addEventListener('scroll', function(event) {
+        var y_pos = window.pageYOffset || document.documentElement.scrollTop;
+        var current_title = document.title;
+        var headerlinks = document.getElementsByClassName('headerlink');
+
+        Array.prototype.forEach.call(headerlinks, function(el, i) {
+            var rect = el.getBoundingClientRect();
+            if (y_pos > rect.top + document.documentElement.scrollTop - 90) {
+                current_title = el.parentElement.textContent. slice(0, -1);
+            }
+        });
+
+        document.title = current_title;
+    });
 })
 
+// Dark mode detection
 !function() {
 	const darkMode = document.getElementById('dark-mode-toggle');
 
@@ -63,16 +81,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	}
 }();
 
-let elements = document.getElementsByClassName("lightgallery")
+// Enable lightgallery
+let elements = document.getElementsByClassName('lightgallery')
 for (let i = 0; i < elements.length; i++) {
   lightGallery(elements[i])
 }
 
-
+// Add an icon to all external links
 var links = document.querySelectorAll( '.md-content a' );
 for (var i = 0, length = links.length; i < length; i++) {
-    if (links[i].hostname != window.location.hostname && links[i].title != "Edit this page") {
+    if (links[i].hostname != window.location.hostname && links[i].title != 'Edit this page') {
         links[i].target = '_blank';
-        links[i].className = "external";
+        links[i].className = 'external';
     }
 }
