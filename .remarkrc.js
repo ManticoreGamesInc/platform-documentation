3ee5af588494b36936df42aa837a3b1812aa189f
Benjamin Staneck <bstaneck@manticoregames.com>
2020-01-19 23:20:39 +0100

    Update remark config

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,37 +1,34 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
 exports.settings = {
-    bullet: "consistent",
     listItemIndent: "1",
     rule: "-",
     ruleRepetition: "3",
     ruleSpaces: false,
     emphasis: "*",
     paddedTable: false
-};
+}
 
-const personalDictionaryPath = path.join(__dirname, ".vscode/spellright.dict");
+const personalDictionaryPath = path.join(__dirname, ".vscode/spellright.dict")
 const personalDictionary = fs.existsSync(personalDictionaryPath)
     ? {
-          personal: fs.readFileSync(personalDictionaryPath, "utf8")
-      }
-    : {};
+        personal: fs.readFileSync(personalDictionaryPath, "utf8")
+    }
+    : {}
 
 exports.plugins = [
-    require("remark-preset-lint-markdown-style-guide"),
-    ["remark-lint-maximum-line-length", 999],
-    ["remark-lint-unordered-list-marker-style", "consistent"],
-    ["remark-lint-ordered-list-marker-style", "consistent"],
-    ["remark-lint-emphasis-marker", "consistent"],
-    ["remark-lint-ordered-list-marker-value", "ordered"],
-    ["remark-lint-no-file-name-irregular-characters", false],
-    ["remark-lint-list-item-spacing", { checkBlanks: true }],
-    ["remark-lint-list-item-indent", "space"],
-    ["remark-lint-list-item-content-indent", false],
-    ["remark-lint-code-block-style", false],
-    ["remark-lint-table-cell-padding", false],
-    ["remark-lint-table-pipe-alignment", false],
-    require("remark-frontmatter"),
-    ["remark-frontmatter", {type: 'custom', marker: '!'}]
-];
+    "preset-lint-markdown-style-guide",
+    ["lint-maximum-line-length", 999],
+    ["lint-emphasis-marker", "consistent"],
+    ["lint-ordered-list-marker-value", "ordered"],
+    ["lint-no-file-name-irregular-characters", false],
+    ["lint-list-item-spacing", { checkBlanks: true }],
+    ["lint-list-item-indent", "space"],
+    ["lint-list-item-content-indent", false],
+    ["lint-code-block-style", false],
+    ["lint-table-cell-padding", false],
+    ["lint-table-pipe-alignment", false],
+    ["lint-no-duplicate-headings", false],
+    ["frontmatter", { type: "custom", marker: "-" }]
+]
