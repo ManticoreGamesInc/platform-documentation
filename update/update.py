199db5dc62c6e6c33bd24066ac448724d01431f8
Benjamin Staneck <bstaneck@manticoregames.com>
2021-06-08 03:46:00 +0200

    Add environment update function to `update` script
    Former-commit-id: 49b67e4a32732d78cf89404cdbac6e1c7b190577

--- update/update.py
+++ update/update.py
@@ -1,6 +1,10 @@
 import requests
+import subprocess
+import sys
+from pathlib import Path
 
 outfilePath = "src/assets/api/CoreLuaAPI.json"
+requirements = str(Path(__file__).parent.parent) + "/requirements.txt"
 
 
 def update_dump(environment="Test"):
@@ -12,3 +16,8 @@ def update_dump(environment="Test"):
     r = requests.get(dumpEndpoint.replace("\r\n", ""))
     open(outfilePath, "wb").write(r.content)
     print(f"Updated JSON Dump to version {latestVersion.text}")
+
+
+def update_environment():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements, "--upgrade"])
+    subprocess.check_call(['npm', "install"])
